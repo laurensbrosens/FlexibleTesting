a73847284bb47e70ab533fe47baec97c258aa06d
brlars <brlaurens@gmail.com>
2026-04-11 00:29:35 +0200

    Added example of how generated code could look like

--- LegacyCodeProject/Viewmodels/UserViewModel.cs
+++ LegacyCodeProject/Viewmodels/UserViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using LegacyCodeProject.Core;
+using System.Runtime.CompilerServices;
+using static LegacyCodeProject.Viewmodels.UserViewModel_g;
 
 namespace LegacyCodeProject.Viewmodels;
 
 public class UserViewModel : BaseViewModel
 {
-    public UserViewModel(SomeDataObject someDataObject) : base(someDataObject)
+    public UserViewModel(SomeDataObject someDataObject)
+        : base(someDataObject)
     {
         Name = "Default";
+        DateTime = DateTime.Now;
     }
 
     public string Name
@@ -19,9 +23,68 @@ public class UserViewModel : BaseViewModel
         }
     }
 
+    public DateTime DateTime { get; set; }
+
     protected override void OnLoad(object? sender, EventArgs e)
     {
         base.OnLoad(sender, e);
         Name = "Test";
+    }
+}
+
+// Example of what the generated class would look like:
+public class UserViewModel_g : BaseViewModel_g //: BaseViewModel
+{
+    // Auto-generated new members:
+    private readonly IAutoDependencies _dependencies;
+
+    public UserViewModel_g(
+        SomeDataObject someDataObject,
+        IAutoDependencies dependencies,
+        IAutoDependenciesBase baseDependencies
+    )
+        : base(someDataObject, baseDependencies)
+    {
+        _dependencies = dependencies;
+        Name = "Default";
+        DateTime = _dependencies.Now();
+    }
+
+    public string Name
+    {
+        get;
+        set
+        {
+            field = value;
+            _dependencies.OnPropertyChanged(); // Huh, I did not think about that, maybe the source generator can copy [CallerMemberName] over?
+        }
+    }
+
+    public DateTime DateTime { get; set; }
+
+    protected override void OnLoad(object? sender, EventArgs e) // Remove override or create virtual in base class? Should be a virtual method in BaseViewModel_g that calls IAutoDependenciesBase.OnLoad
+    {
+        base.OnLoad(sender, e); // Of _dependencies.OnLoad(sender, e);?
+        Name = "Test";
+    }
+
+    public interface IAutoDependencies
+    {
+        Func<DateTime> Now { get; }
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null!); // Is this possible for a sourcegenerator?
+    }
+
+    public interface IAutoDependenciesBase
+    {
+        void OnLoad(object? sender, EventArgs e);
+    }
+}
+
+public class BaseViewModel_g(SomeDataObject someDataObject, IAutoDependenciesBase dependencies)
+{
+    protected virtual void OnLoad(object? sender, EventArgs e)
+    {
+        dependencies.OnLoad(sender, e);
     }
 }
---
+++ LegacyCodeProjectTests/UserViewModelBuilder.cs
@@ -0,0 +1,59 @@
+﻿using LegacyCodeProject.Viewmodels;
+using System.Linq.Expressions;
+
+namespace LegacyCodeProjectTests;
+
+[GeneratorInstructions]
+public class UserViewModelBuilder : IGeneratorInstructions
+{
+    public static void Configure()
+    {
+        Overwrites.ForClass<UserViewModel>();
+
+        Overwrites.Replace<Func<DateTime>>(
+            () => DateTime.Now,
+            () => TestClock.Now);
+
+        Overwrites.Replace<Func<string, string>>(
+            path => File.ReadAllText(path),
+            path => TestFile.ReadAllText(path));
+
+        Overwrites.Replace<Func<string, bool>>(
+            s => s.IsValidEmail(),
+            s => TestEmail.IsValidEmail(s));
+
+        Overwrites.RedirectNew<SomeService, Func<string, SomeService>>(); // To overwrite things like "new SomeService" that has side-effects in it's constructor, could be combined with Mockable?
+
+        Overwrites.MockInheritance(); // Automatically create a fake base class if needed, could be combined with InheritFrom?
+
+        Overwrites.InheritFrom<FakeBaseViewModel>(); // Developer provided fake base, usefull for base classes that are used a lot
+
+        // Or make it mockable:
+        Overwrites.Mockable<Func<string, string>>(path => File.ReadAllText(path));
+
+        Overwrites.Mockable<Func<DateTime>>(() => DateTime.Now);
+    }
+
+    public static class TestClock
+    {
+        public static DateTime Now => new DateTime(2000, 1, 1);
+    }
+
+    public static class TestFile
+    {
+        public static string ReadAllText(string path) => "Test content";
+    }
+
+    public static class TestEmail
+    {
+        public static bool IsValidEmail(string email) => true;
+    }
+}
+
+public static class Overwrites
+{
+    public static void Replace<TDelegate>(Expression<TDelegate> target, Expression<TDelegate> replacement)
+        where TDelegate : Delegate
+    { }
+    // Etc. for the others
+}
