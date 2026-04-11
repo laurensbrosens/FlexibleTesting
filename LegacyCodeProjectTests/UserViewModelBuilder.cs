a54789916b661f99bcaa263b4e8837f6e80ab810
brlars <brlaurens@gmail.com>
2026-04-12 00:44:07 +0200

    Wip testing how to expose protected methods

--- LegacyCodeProjectTests/UserViewModelBuilder.cs
+++ LegacyCodeProjectTests/UserViewModelBuilder.cs
@@ -1,26 +1,39 @@
-﻿using LegacyCodeProject.Viewmodels;
+﻿using LegacyCodeProject.Core;
+using LegacyCodeProject.Viewmodels;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 namespace LegacyCodeProjectTests;
 
 [GeneratorInstructions]
-public class UserViewModelBuilder : IGeneratorInstructions
+public class UserViewModelBuilder//(SomeDataObject someDataObject) : UserViewModel(someDataObject), IGeneratorInstructions
 {
+    /*
+    public class Accessor(SomeDataObject someDataObject) : UserViewModel(someDataObject)
+    {
+        public static string MethodProvider()
+        {
+            return nameof(OnLoad);
+        }
+
+        /*public static void MakePublic<TTarget, TDelegate>(Expression<TDelegate> selector)
+            where TDelegate : Delegate { }*/
+
+        public static Expression<TDelegate> MakePublic<TTarget, TDelegate>()
+            where TDelegate : Delegate { 
+            return (sender, e) => ((UserViewModelBuilder)null!).OnLoad(sender, e);
+        }
+    }*/
+
     public static void Configure()
     {
         Overwrites.ForClass<UserViewModel>();
 
-        Overwrites.Replace<Func<DateTime>>(
-            () => DateTime.Now,
-            () => TestClock.Now);
+        Overwrites.Replace<Func<DateTime>>(() => DateTime.Now, () => TestClock.Now);
 
-        Overwrites.Replace<Func<string, string>>(
-            path => File.ReadAllText(path),
-            path => TestFile.ReadAllText(path));
+        Overwrites.Replace<Func<string, string>>(path => File.ReadAllText(path), path => TestFile.ReadAllText(path));
 
-        Overwrites.Replace<Func<string, bool>>(
-            s => s.IsValidEmail(),
-            s => TestEmail.IsValidEmail(s));
+        Overwrites.Replace<Func<string, bool>>(s => s.IsValidEmail(), s => TestEmail.IsValidEmail(s));
 
         Overwrites.RedirectNew<SomeService, Func<string, SomeService>>(); // To overwrite things like "new SomeService" that has side-effects in it's constructor, could be combined with Mockable?
 
@@ -32,28 +45,81 @@ public class UserViewModelBuilder : IGeneratorInstructions
         Overwrites.Mockable<Func<string, string>>(path => File.ReadAllText(path));
 
         Overwrites.Mockable<Func<DateTime>>(() => DateTime.Now);
-    }
 
-    public static class TestClock
-    {
-        public static DateTime Now => new DateTime(2000, 1, 1);
-    }
+        Overwrites.MakePublic(nameof(UserViewModelBuilder.OnLoad));
+        Overwrites.MakePublic(Accessor.MethodProvider());
 
-    public static class TestFile
-    {
-        public static string ReadAllText(string path) => "Test content";
-    }
+        //[UnsafeAccessorType(nameof(UserViewModel))]
 
-    public static class TestEmail
-    {
-        public static bool IsValidEmail(string email) => true;
+        Overwrites.MakePublic<UserViewModel, Action<UserViewModel, object?, EventArgs>>((vm, sender, e) => UserViewModelAccessors.OnLoad(vm, sender, e));
     }
+    internal static class UserViewModelAccessors
+    {
+        // Name omitted: by default, the accessor method name "OnLoad"
+        // is used as the target member name.
+        [UnsafeAccessor(UnsafeAccessorKind.Method)]
+        internal static extern void OnLoad(UserViewModel instance, object? sender, EventArgs e);
+    }
+}
+
+
+
+internal class FakeBaseViewModel { }
+
+internal class GeneratorInstructionsAttribute : Attribute { }
+
+public interface IGeneratorInstructions { }
+
+public static class TestClock
+{
+    public static DateTime Now => new DateTime(2000, 1, 1);
+}
+
+public static class TestFile
+{
+    public static string ReadAllText(string path) => "Test content";
+}
+
+public static class TestEmail
+{
+    public static bool IsValidEmail(string email) => true;
 }
 
 public static class Overwrites
 {
     public static void Replace<TDelegate>(Expression<TDelegate> target, Expression<TDelegate> replacement)
-        where TDelegate : Delegate
-    { }
+        where TDelegate : Delegate { }
+
     // Etc. for the others
+
+    public static void MakePublic(string methodName, params Type[] parameterTypes) { }
+
+    internal static void ForClass<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void MockInheritance()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void InheritFrom<T>()
+    {
+        throw new NotImplementedException();
+    }
+
+    /*
+    internal static void Mockable<T>(Expression<TDelegate> value) where TDelegate : Delegate
+    {
+        throw new NotImplementedException();
+    }
+    */
+    public static void MakePublic<TTarget, TDelegate>(Expression<TDelegate> selector)
+        where TDelegate : Delegate { }
+
+    internal static void RedirectNew<T1, T2>()
+    {
+        throw new NotImplementedException();
+    }
 }
---
+++ implementation.md
@@ -0,0 +1,3 @@
+Om dingen makkelijk te maken, best simpel beginnen. Stappenplan:
+1. Maak een kopie van de class via Overwrites.ForClass<ClassToCopy>(); in een .g.cs file en zet een _g in de classname
+2. 
