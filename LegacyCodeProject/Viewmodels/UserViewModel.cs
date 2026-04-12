a39c775ea0e44b28ba6968ed52c5d3525f27bf37
brlars <brlaurens@gmail.com>
2026-04-12 20:33:16 +0200

    Functional code, unfortunatly this only works if the generated code is in the same project as the legacy class

--- LegacyCodeProject/Viewmodels/UserViewModel.cs
+++ LegacyCodeProject/Viewmodels/UserViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using LegacyCodeProject.Core;
-using System.Runtime.CompilerServices;
 
 namespace LegacyCodeProject.Viewmodels;
 
@@ -9,7 +8,7 @@ public class UserViewModel : BaseViewModel
         : base(someDataObject)
     {
         Name = "Default";
-        DateTime = DateTime.Now;
+        DateTime = DateTime.Now; // Test
         _userService = new UserService();
     }
 
@@ -32,63 +31,4 @@ public class UserViewModel : BaseViewModel
     }
 
     private UserService _userService;
-}
-/*
-// Example of what the generated class would look like:
-public class UserViewModel_g : BaseViewModel_g //: BaseViewModel
-{
-    // Auto-generated new members:
-    private readonly IAutoDependencies _dependencies;
-
-    public UserViewModel_g(SomeDataObject someDataObject, IAutoDependencies dependencies, IAutoDependenciesBase baseDependencies)
-        : base(someDataObject, baseDependencies)
-    {
-        _dependencies = dependencies;
-        Name = "Default";
-        DateTime = _dependencies.Now();
-        _userService = _dependencies.UserService;
-    }
-
-    public string Name
-    {
-        get;
-        set
-        {
-            field = value;
-            _dependencies.OnPropertyChanged();
-        }
-    }
-
-    public DateTime DateTime { get; set; }
-
-    protected override void OnLoad(object? sender, EventArgs e)
-    {
-        base.OnLoad(sender, e); // Or _dependencies.OnLoad(sender, e);?
-        Name = "Test";
-    }
-
-    private IUserService _userService;
-
-
-}
-*/    
-public interface IAutoDependencies
-    {
-        Func<DateTime> Now { get; }
-        IUserService UserService { get; }
-        void OnPropertyChanged([CallerMemberName] string propertyName = null!); // Note, the generator has to check for [CallerMemberName]!
-    }
-
-    public interface IAutoDependenciesBase
-    {
-        void OnLoad(object? sender, EventArgs e);
-    }
-public interface IUserService { }
-
-public class BaseViewModel_g(SomeDataObject someDataObject, IAutoDependenciesBase dependencies)
-{
-    protected virtual void OnLoad(object? sender, EventArgs e)
-    {
-        dependencies.OnLoad(sender, e);
-    }
 }
--- LegacyCodeProjectTests/UserViewModelTests.cs
+++ LegacyCodeProjectTests/UserViewModelTests.cs
@@ -1,4 +1,5 @@
 ﻿using LegacyCodeProject.Core;
+using LegacyCodeProject.Viewmodels;
 
 namespace LegacyCodeProjectTests;
 
@@ -49,3 +50,65 @@ public class UserViewModelTests
         Assert.That(loadEventFired, Is.True);
     }
 }
+/* Old code
+
+
+// Example of what the generated class would look like:
+public class UserViewModel_g : BaseViewModel_g //: BaseViewModel
+{
+    // Auto-generated new members:
+    private readonly IAutoDependencies _dependencies;
+
+    public UserViewModel_g(SomeDataObject someDataObject, IAutoDependencies dependencies, IAutoDependenciesBase baseDependencies)
+        : base(someDataObject, baseDependencies)
+    {
+        _dependencies = dependencies;
+        Name = "Default";
+        DateTime = _dependencies.Now();
+        _userService = _dependencies.UserService;
+    }
+
+    public string Name
+    {
+        get;
+        set
+        {
+            field = value;
+            _dependencies.OnPropertyChanged();
+        }
+    }
+
+    public DateTime DateTime { get; set; }
+
+    protected override void OnLoad(object? sender, EventArgs e)
+    {
+        base.OnLoad(sender, e); // Or _dependencies.OnLoad(sender, e);?
+        Name = "Test";
+    }
+
+    private IUserService _userService;
+
+
+}
+
+public interface IAutoDependencies
+{
+    Func<DateTime> Now { get; }
+    IUserService UserService { get; }
+    void OnPropertyChanged([CallerMemberName] string propertyName = null!); // Note, the generator has to check for [CallerMemberName]!
+}
+
+public interface IAutoDependenciesBase
+{
+    void OnLoad(object? sender, EventArgs e);
+}
+public interface IUserService { }
+
+public class BaseViewModel_g(SomeDataObject someDataObject, IAutoDependenciesBase dependencies)
+{
+    protected virtual void OnLoad(object? sender, EventArgs e)
+    {
+        dependencies.OnLoad(sender, e);
+    }
+}
+*/
