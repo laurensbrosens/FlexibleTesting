5d07079e418db3b6e5c399fd91040d19420185b4
brlars <brlaurens@gmail.com>
2026-04-15 14:48:25 +0200

    Created copy for instruction utilities in test project for now

---
+++ FlexibleTesting.Tasks/GeneratorInstructionsAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FlexibleTesting;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class GeneratorInstructionsAttribute : Attribute
+{
+    public Type[] InterfacesTypes { get; }
+    public GeneratorInstructionsAttribute(params Type[] interfacesTypes)
+    {
+        InterfacesTypes = interfacesTypes;
+    }
+}
