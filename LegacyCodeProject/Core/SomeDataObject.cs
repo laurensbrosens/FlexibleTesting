a35b7acce22954ed21c5ffed7d294d05c0931655
brlars <brlaurens@gmail.com>
2026-04-22 00:42:38 +0200

    Removed old test code

--- LegacyCodeProject/Core/SomeDataObject.cs
+++ LegacyCodeProject/Core/SomeDataObject.cs
@@ -1,15 +1,7 @@
-﻿
-using LegacyCodeProject.Viewmodels;
-
-namespace LegacyCodeProject.Core;
+﻿namespace LegacyCodeProject.Core;
 
 public class SomeDataObject
 {
     public int MyProperty { get; set; }
     public int MyProperty1 { get; set; }
-    public void Test()
-    {
-        // var test = new UserViewModel_G(this);
-        var test2 = new UserViewModel(this);
-    }
 }
